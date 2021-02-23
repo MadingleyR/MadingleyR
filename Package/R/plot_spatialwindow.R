38ccb7016313fdf0bb01293c54acc0bc0eb6fe75
MadingleyR <mad@pop.local>
2021-02-23 20:32:53 +0100

    package functions updated (plotting)

--- Package/R/plot_spatialwindow.R
+++ Package/R/plot_spatialwindow.R
@@ -1,4 +1,4 @@
-plot_spatialwindow = function(spatial_window,raster=0,line_color="red"){
+plot_spatialwindow = function(spatial_window, raster=0, window_color="red",...){
 
   if(length(spatial_window)!=4) {
     stop("Please provide 4 coordinates c(min_long, max_long, min_lat, max_lat)")
@@ -24,9 +24,9 @@ plot_spatialwindow = function(spatial_window,raster=0,line_color="red"){
     raster[raster[]==1] = NA
   }
   
-  plot(raster,colNA="grey",legend=FALSE)
-  lines(x=c(spatial_window[[1]],spatial_window[[1]]),y=c(spatial_window[[3]],spatial_window[[4]]), col = line_color) # vert left
-  lines(x=c(spatial_window[[2]],spatial_window[[2]]),y=c(spatial_window[[3]],spatial_window[[4]]), col = line_color) # vert right
-  lines(x=c(spatial_window[[1]],spatial_window[[2]]),y=c(spatial_window[[4]],spatial_window[[4]]), col = line_color) # vert right
-  lines(x=c(spatial_window[[1]],spatial_window[[2]]),y=c(spatial_window[[3]],spatial_window[[3]]), col = line_color) # vert right
+  plot(raster,colNA="grey",legend=FALSE,...)
+  lines(x=c(spatial_window[[1]],spatial_window[[1]]),y=c(spatial_window[[3]],spatial_window[[4]]), col = window_color) # vert left
+  lines(x=c(spatial_window[[2]],spatial_window[[2]]),y=c(spatial_window[[3]],spatial_window[[4]]), col = window_color) # vert right
+  lines(x=c(spatial_window[[1]],spatial_window[[2]]),y=c(spatial_window[[4]],spatial_window[[4]]), col = window_color) # vert right
+  lines(x=c(spatial_window[[1]],spatial_window[[2]]),y=c(spatial_window[[3]],spatial_window[[3]]), col = window_color) # vert right
 }
