0bea443ec7028ed5aa6b6b5ac9b6ed7604876dac
MadingleyR <mad@pop.local>
2021-03-01 20:50:45 +0100

    vignette update

--- Package/R/madingley_inputs.R
+++ Package/R/madingley_inputs.R
@@ -1,4 +1,4 @@
-madingley_inputs = function(input_type = "print options", use_rasters = FALSE) {
+madingley_inputs = function(input_type = "print options") {
 
   input_options = c("spatial inputs","cohort definition","stock definition","model parameters","print options")
 
@@ -30,72 +30,62 @@ madingley_inputs = function(input_type = "print options", use_rasters = FALSE) {
       require(raster)
     }
     
-    # use if loading .rds file fails...
-    if(use_rasters){
-  
-      spatial_path=paste0(get_lib_path(),"/spatial_input_rasters")
-  
-  
-      file_names = list.files(spatial_path,full.names = T,recursive = T)
-      list_names = gsub("\\..*","",list.files(spatial_path,full.names = F,recursive = T))
-  
-      FILES_sp = c("realm_classification","land_mask","hanpp","available_water_capacity",
-                   "Ecto_max","Endo_C_max","Endo_H_max","Endo_O_max")
-      FILES_sp_temp = c("terrestrial_net_primary_productivity","near-surface_temperature",
-                        "precipitation","ground_frost_frequency","diurnal_temperature_range")
-  
-      # check if all files are there
-      for(i in FILES_sp){
-        if(length(grep(i,file_names,value = T))!=1){
-          stop("Could not find raster: ",i,".tif \n")
-        }
+
+    spatial_path=paste0(get_lib_path(),"/spatial_input_rasters")
+
+
+    file_names = list.files(spatial_path,full.names = T,recursive = T)
+    list_names = gsub("\\..*","",list.files(spatial_path,full.names = F,recursive = T))
+
+    FILES_sp = c("realm_classification","land_mask","hanpp","available_water_capacity",
+                 "Ecto_max","Endo_C_max","Endo_H_max","Endo_O_max")
+    FILES_sp_temp = c("terrestrial_net_primary_productivity","near-surface_temperature",
+                      "precipitation","ground_frost_frequency","diurnal_temperature_range")
+
+    # check if all files are there
+    for(i in FILES_sp){
+      if(length(grep(i,file_names,value = T))!=1){
+        stop("Could not find raster: ",i,".tif \n")
       }
-      for(i in FILES_sp_temp){
-        if(length(grep(i,file_names,value = T))!=12){
-          stop("Could not find raster all 12 monthly rasters containing data on: ",i,"\n")
-        }
-      }
-  
-      cat('Reading default input rasters from: ',spatial_path)
-  
-      # read sp non-temp files
-      for(i in FILES_sp){
-        file_name = grep(i,file_names,value = T)
-        cat(".")
-        output[[i]] = raster(file_name)
-      }
-  
-      # read sp temp files
-      for(i in FILES_sp_temp){
-        file_name = grep(i,file_names,value = T)
-  
-        # sort files
-        if(length(grep("_1.tif",file_name,value = T))==0){
-          file_name_sort = file_name
-        }else{
-          if(substr(spatial_path,nchar(spatial_path),nchar(spatial_path))=="/"){
-            file_name_sort = paste0(spatial_path,i,"_",1:12)
-          }else{
-            file_name_sort = paste0(spatial_path,"/",i,"_",1:12,".tif")
-          }
-        }
-        if(length(file_name_sort)==12){
-          output[[i]] = brick(lapply(file_name_sort,raster))
-        }
-        cat(".")
-      }
-      cat("\n")
-  
-      return(output)
-    }else{
-      
-      cat('Reading default spatial inputs from .rds file')
-      
-      # standard loading of spatial files using .rds (quicker)
-      spatial_path=paste0(get_lib_path(),"/SpatialInputs.rds")
-      return(readRDS(spatial_path))
-      
     }
+    for(i in FILES_sp_temp){
+      if(length(grep(i,file_names,value = T))!=12){
+        stop("Could not find raster all 12 monthly rasters containing data on: ",i,"\n")
+      }
+    }
+
+    cat('Reading default input rasters from: ',spatial_path)
+
+    # read sp non-temp files
+    for(i in FILES_sp){
+      file_name = grep(i,file_names,value = T)
+      cat(".")
+      output[[i]] = raster(file_name)
+    }
+
+    # read sp temp files
+    for(i in FILES_sp_temp){
+      file_name = grep(i,file_names,value = T)
+
+      # sort files
+      if(length(grep("_1.tif",file_name,value = T))==0){
+        file_name_sort = file_name
+      }else{
+        if(substr(spatial_path,nchar(spatial_path),nchar(spatial_path))=="/"){
+          file_name_sort = paste0(spatial_path,i,"_",1:12)
+        }else{
+          file_name_sort = paste0(spatial_path,"/",i,"_",1:12,".tif")
+        }
+      }
+      if(length(file_name_sort)==12){
+        output[[i]] = brick(lapply(file_name_sort,raster))
+      }
+      cat(".")
+    }
+    cat("\n")
+
+    return(output)
+
 
   }else if(input_type=="cohort definition"){
 
