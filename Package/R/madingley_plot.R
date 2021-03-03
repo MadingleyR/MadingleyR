b88878e5ccca5222a78e7fbe968985d01a113eba
MadingleyR <78355923+MadingleyR@users.noreply.github.com>
2021-03-03 20:36:13 +0100

    Update madingley_plot.R

--- Package/R/madingley_plot.R
+++ Package/R/madingley_plot.R
@@ -1,21 +1,21 @@
 madingley_plot = function(madingley_data){
-
+  
   # store default par
   def_par = par(no.readonly = TRUE)
-
-  layout(matrix(c(7, 7, 7, 7, 7, 7, 1, 2, 3, 4, 5, 6), nrow=4, byrow=TRUE))
-
-  plot_densities(madingley_data, weighted = 'biomass', xlim = c(-4, 5), multipanel = F)
+  
+  # try to make plots
+  try(plot_timelines(madingley_data, select = 'feeding guilds', main="Time series plot"))
   cat('\n')
-  plot_timelines(madingley_data, select = 'feeding guilds', main="Time series plot")
-
-  # return to default par
-  par(def_par)
-
+  try(plot_densities(madingley_data, weighted = 'biomass', xlim = c(-4, 5), multipanel = TRUE))
+  cat('\n')
   try(plot_foodweb(madingley_data, max_flows = 5, sample_data = 30, main="Food-web plot"))
   cat('\n')
   try(plot_trophicpyramid(madingley_data, main="Trophic Pyramid"))
   cat('\n')
-  try(plot_spatialbiomass(madingley_data, functional_filter = T, box=F, axes=F))
-
+  try(plot_spatialbiomass(madingley_data, functional_filter = TRUE, box=FALSE, axes=FALSE))
+  
+  # return to default par
+  par(def_par)
+  
 }
+
