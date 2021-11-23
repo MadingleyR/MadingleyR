2c82db1af3dad9cdab2ce97e82be269e39c3011d
MadingleyR <Madingley_R_package@outlook.com>
2021-11-23 09:13:52 +0100

    done with hanpp?

--- Tests/HANPP_tests.r
+++ Tests/HANPP_tests.r
@@ -1,31 +1,30 @@
-# Load the remotes package
-# library('remotes') # or use library('devtools')
-
-# Install the MadingleyR package
-#install_github('MadingleyR/MadingleyR', subdir='Package', force = TRUE)
+#detach("package:MadingleyR", unload = TRUE) # unload if loaded, don't mind the error
+#remove.packages("MadingleyR")  # uninstall package
+#library('remotes') # or use library('devtools') # load the remotes package
+#install_github('MadingleyR/MadingleyR', subdir='Package', force = TRUE, build_vignettes = TRUE) # install the MadingleyR package
 library(MadingleyR)
+madingley_version( ) # check pkg = 1.0.2, C++ src = 2.02
 
 # Set model params
-spatial_window = c(35, 37, 50, 52) # region of interest, somewhere in europe
+spatial_window = c(35, 37, 50, 52) # region of interest
 plot_spatialwindow(spatial_window)
-sptl_inp = madingley_inputs('spatial inputs') # load default inputs
+sp_in = madingley_inputs('spatial inputs') # load default inputs
 
 # Initialise model
-mdata = madingley_init(spatial_window = spatial_window, spatial_inputs = sptl_inp)
+mdata = madingley_init(spatial_window = spatial_window, spatial_inputs = sp_in)
 
-# Run spin-up of 10 years 
-mdata2 = madingley_run(madingley_data = mdata, spatial_inputs = sptl_inp, years = 10, apply_hanpp = 0)
+# Run model of 10 years, without veg reduction
+mdata2 = madingley_run(madingley_data = mdata, spatial_inputs = sp_in, years = 10, apply_hanpp = 0) # apply_hanpp = 0, hanpp raster not used
+
+# Run model of 10 years, reduce veg by 50%
+sp_in$hanpp[] = 0.5 # this sets all the values of the hanpp raster to 0.5, thus applying the reduction on all cells
+mdata3 = madingley_run(madingley_data = mdata, spatial_inputs = sp_in, years = 10, apply_hanpp = 1) # apply_hanpp = 1, fractional raster applied
+
+# Run model of 10 years, reduce veg by 75%
+sp_in$hanpp[] = 0.25 # this sets all the values of the hanpp raster to 0.25, thus reducing the veg production by 0.75
+mdata4 = madingley_run(madingley_data = mdata, spatial_inputs = sp_in, years = 10, apply_hanpp = 1) # apply_hanpp = 1, fractional raster applied
+
+# Check the veg reduction, sometimes more years are needed to arrived at the right values
 mean(mdata2$time_line_stocks$TotalStockBiomass[mdata2$time_line_stocks$Year==10])
-
-# Run spin-up of 10 years 
-sptl_inp$hanpp[] = 0.5
-mdata3 = madingley_run(madingley_data = mdata, spatial_inputs = sptl_inp, years = 10, apply_hanpp = 1)
 mean(mdata3$time_line_stocks$TotalStockBiomass[mdata3$time_line_stocks$Year==10])
-
-# Run spin-up of 10 years 
-sptl_inp$hanpp[] = 0.25
-mdata4 = madingley_run(madingley_data = mdata, spatial_inputs = sptl_inp, years = 10, apply_hanpp = 1)
 mean(mdata4$time_line_stocks$TotalStockBiomass[mdata4$time_line_stocks$Year==10])
-
-
-madingley_version()
